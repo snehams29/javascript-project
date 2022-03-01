37f477d05f3a9093b15f488cf05f217fae3600ab
sangamone <98866361+snehams29@users.noreply.github.com>
2022-03-01 11:18:12 +0530

    Delete adding numbers.js

--- adding numbers.js
+++
@@ -1,9 +0,0 @@
-//Add two numbers entered by the user
-
-const num1=parseInt(prompt('enter the first number'));
-const num2=parseInt(prompt('enter the second number'));
-//add two number
-const sum=num1+num2;
-
-//display the sum
-console.log(`The sum of ${num1} and ${num2} is ${sum}`);
